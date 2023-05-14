6ce283a754807f2895f8aacbf50a11a43f0b900f
MayankPhate <131446991+MayankPhate@users.noreply.github.com>
2023-05-14 15:40:20 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Prakash Sharma";
+            document.title = "Portfolio | Mayank Phate";
             $("#favicon").attr("href", "assets/images/pofile3-.png");
         }
         else {
